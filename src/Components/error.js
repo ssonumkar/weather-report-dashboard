2792d21f5e6627417ff0e1e12bd9eb7dcf5a962f
Sagar Sonumkar <sagarsonumkar@gmail.com>
2023-07-22 11:40:07 +0530

    updated error handling

---
+++ src/Components/error.js
@@ -0,0 +1,13 @@
+export function handleError(error, navigate, message) {
+    console.error("Request failed: ", message, ": ", error)
+    switch (error.response.status) {
+        case 401:      
+            alert(message,': Unauthorized access.');
+            navigate("/")
+            break;
+    
+        default: console.error(message, ": ", error, "Please try again.");
+            break;
+    }
+    
+}
